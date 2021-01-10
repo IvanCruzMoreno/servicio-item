4fa8b8191cf0eb90038c339a0000c6f003960f6a
ivan <ivanmoreno405@gmail.com>
2021-01-10 11:28:44 -0600

    i implemented hystrix to apply circuit breakers to a potentially failed method

--- servicio-item/src/main/java/com/badbadcode/application/controllers/ItemController.java
+++ servicio-item/src/main/java/com/badbadcode/application/controllers/ItemController.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.badbadcode.application.models.entity.Item;
+import com.badbadcode.application.models.entity.Producto;
 import com.badbadcode.application.models.service.ItemService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class ItemController {
@@ -22,9 +24,21 @@ public class ItemController {
 	public List<Item> listar(){
 		return itemService.findAll();
 	}
+	@HystrixCommand(fallbackMethod = "metodoAlternativo")
 	@GetMapping("/ver/{id}/cantidad/{cantidad}")
 	public Item detalle(@PathVariable Long id, @PathVariable Integer cantidad) {
 		return itemService.findById(id, cantidad);
+	}
+	public Item metodoAlternativo(Long id, Integer cantidad) {
+		Item item = new Item();
+		Producto producto = new Producto();
+		
+		item.setCantidad(cantidad);
+		producto.setId(id);
+		producto.setNombre("Bocina");
+		producto.setPrecio(100.00);
+		item.setProducto(producto);
+		return item;
 	}
 	
 }
