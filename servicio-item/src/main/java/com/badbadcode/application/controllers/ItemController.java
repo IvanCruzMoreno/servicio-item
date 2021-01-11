690bbb663d23a9fa2e350f0c2f2e81a5fd745495
ivan <ivanmoreno405@gmail.com>
2021-01-11 10:31:47 -0600

    these changes are to have some profiles, for example "produccion" or "desarrollo" - they are nested with config-sever

--- servicio-item/src/main/java/com/badbadcode/application/controllers/ItemController.java
+++ servicio-item/src/main/java/com/badbadcode/application/controllers/ItemController.java
@@ -1,9 +1,17 @@
 package com.badbadcode.application.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +24,11 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 @RestController
 public class ItemController {
 
+	private static Logger log = LoggerFactory.getLogger(ItemController.class);
+	
+	@Autowired
+	private Environment enviroment;
+	
 	@Autowired
 	@Qualifier("serviceFeign")
 	private ItemService itemService;
@@ -40,5 +53,35 @@ public class ItemController {
 		item.setProducto(producto);
 		return item;
 	}
+	@GetMapping("/obtener-config")
+	public ResponseEntity<?> obtenerConfig(@Value("${server.port}") String puerto, @Value("${configuracion.texto}") String texto){
+		
+		log.info(texto);
+		
+		Map<String, String> json = new HashMap<>();
+		json.put("texto", texto);
+		json.put("puerto", puerto);
+		
+		if(enviroment.getActiveProfiles().length > 0 && enviroment.getActiveProfiles()[0].equals("dev")) {
+			json.put("autor.nombre", enviroment.getProperty("configuracion.autor.nombre"));
+			json.put("autor.email", enviroment.getProperty("configuracion.autor.email"));
+		}
+		
+		return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
+
+	}
 	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
